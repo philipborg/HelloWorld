a5dcf4af93621d2c6c006f440eb7f628976544b1
warpech <warpech@gmail.com>
2016-04-26 22:08:33 +0200

    step 5: add simple expense tracking
    - use partials to compose UI
    - use calculated properties on the data model

--- src/HelloWorld/PersonJson.json.cs
+++ src/HelloWorld/PersonJson.json.cs
@@ -4,11 +4,25 @@ namespace HelloWorld
 {
     partial class PersonJson : Json
     {
+        static PersonJson()
+        {
+            DefaultTemplate.Expenses.ElementType.InstanceType = typeof(ExpenseJson);
+        }
+
         public string FullName => FirstName + " " + LastName;
 
         void Handle(Input.SaveTrigger action)
         {
             Transaction.Commit();
+        }
+
+        void Handle(Input.NewExpenseTrigger action)
+        {
+            new Expense()
+            {
+                Spender = this.Data as Person,
+                Amount = 1
+            };
         }
     }
 }
