67c23e79dce140d3c48ddf1633cf7d2d181952ed
warpech <warpech@gmail.com>
2016-04-26 15:08:51 +0200

    step 1: smallest app that creates a persistent object on startup

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Starcounter;
+
+namespace HelloWorld {
+    [Database]
+    public class Person {
+        public string FirstName;
+        public string LastName;
+    }
+
+    class Program {
+        static void Main() {
+            Db.Transact(() => {
+                new Person() {
+                    FirstName = "Johnny",
+                    LastName = "Doe"
+                };
+            });
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# HelloWorld
+Sample Starcounter application that shows CRUD operations with Puppet (HTML+JSON-Patch client)
+
+![](./screenshot.png)
