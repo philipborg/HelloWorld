9f2dfc26d20a58c0fcd619b5e76cbc2f11a847fd
warpech <warpech@gmail.com>
2016-04-26 15:08:51 +0200

    step 1: smallest app that creates a persistent object on startup

---
+++ src/HelloWorld/Program.cs
@@ -0,0 +1,30 @@
+﻿using Starcounter;
+
+namespace HelloWorld
+{
+    [Database]
+    public class Person
+    {
+        public string FirstName;
+        public string LastName;
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Db.Transact(() =>
+            {
+                var anyone = Db.SQL<Person>("SELECT p FROM Person p").First;
+                if (anyone == null)
+                {
+                    new Person
+                    {
+                        FirstName = "John",
+                        LastName = "Doe"
+                    };
+                }
+            });
+        }
+    }
+}
