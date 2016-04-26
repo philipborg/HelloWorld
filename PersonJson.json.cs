9483dfad38e1ef85c25eb303e39c8b1236406244
warpech <warpech@gmail.com>
2016-04-26 22:27:55 +0200

    step 6: add a cancel button and a delete all button

--- PersonJson.json.cs
+++ PersonJson.json.cs
@@ -8,15 +8,22 @@ namespace HelloWorld {
             Transaction.Commit();
         }
 
-        void Handle(Input.AddNewExpense action) {
-            Transaction.Commit();
+        void Handle(Input.Cancel action) {
+            Transaction.Rollback();
+        }
 
+        void Handle(Input.AddNewExpense action) {
             var expense = new Expense() {
                 Spender = (Person)this.Data,
                 Amount = 1
             };
             var expenseJson = Self.GET("/HelloWorld/partial/expense/" + expense.GetObjectID());
             this.Expenses.Add(expenseJson);
+        }
+
+        void Handle(Input.DeleteAll action) {
+            Db.SlowSQL("DELETE FROM Expense WHERE Spender = ?", this.Data);
+            this.Expenses.Clear();
         }
     }
 }
