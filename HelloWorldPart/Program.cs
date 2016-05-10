bc7b0c11b17dc23cf145b6e0b66ee07b011fd642
aloscha <oleksiy.yatsko@gmail.com>
2016-05-10 11:39:08 +0200

    Add part project

---
+++ HelloWorldPart/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Starcounter;
+
+namespace HelloWorldPart {
+    class Program {
+        static void Main() {
+            Handle.GET("/helloworldpart/partials/helloworldpartannouncment/{?}", (string objectId) =>
+            {
+                var page = Self.GET("/helloworldpart/partials/helloworldpart/" + objectId);
+                return page;
+            });
+
+            Handle.GET("/helloworldpart/partials/helloworldpart/{?}", (string objectId) => null);
+
+            UriMapping.OntologyMap("/helloworldpart/partials/helloworldpartannouncment/@w", "simplified.ring1.Illustration", null, null);
+            UriMapping.OntologyMap("/helloworldpart/partials/helloworldpart/@w", "simplified.ring1.Somebody", objectId => objectId, objectId => null);
+        }
+    }
+}
