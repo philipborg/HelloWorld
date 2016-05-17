41dda735f830ceb630bd91ce2e77af2824a548ee
aloscha <oleksiy.yatsko@gmail.com>
2016-05-17 16:55:20 +0200

    Add session handling

---
+++ HelloWorldPart/JsonWithSubPage.json.cs
@@ -0,0 +1,8 @@
+using Starcounter;
+
+namespace HelloWorldPart
+{
+    partial class JsonWithSubPage : Json
+    {
+    }
+}
--- HelloWorldPart/Program.cs
+++ HelloWorldPart/Program.cs
@@ -1,19 +1,21 @@
-﻿using System;
-using Starcounter;
+﻿using Starcounter;
 
 namespace HelloWorldPart {
     class Program {
         static void Main() {
             Handle.GET("/helloworldpart/partials/helloworldpartannouncment/{?}", (string objectId) =>
             {
-                var page = Self.GET("/helloworldpart/partials/helloworldpart/" + objectId);
+                var page = new JsonWithSubPage();
+                page.SubPage = Self.GET("/helloworldpart/partials/helloworldpart/" + objectId);
                 return page;
             });
 
-            Handle.GET("/helloworldpart/partials/helloworldpart/{?}", (string objectId) => null);
+            Handle.GET("/helloworldpart/partials/helloworldpart/{?}", (string objectId) => {
+                return new Page();
+            });
 
-            UriMapping.OntologyMap("/helloworldpart/partials/helloworldpartannouncment/@w", "simplified.ring1.Illustration", null, null);
-            UriMapping.OntologyMap("/helloworldpart/partials/helloworldpart/@w", "simplified.ring1.Somebody", objectId => objectId, objectId => null);
+            UriMapping.OntologyMap("/helloworldpart/partials/helloworldpartannouncment/@w", "simplified.ring1.Illustration");
+            UriMapping.OntologyMap("/helloworldpart/partials/helloworldpart/@w", "simplified.ring1.Somebody");
         }
     }
 }
