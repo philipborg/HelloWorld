b00a214b4b47f100069022faef1df2c41b66fd0c
warpech <warpech@gmail.com>
2016-05-17 18:52:15 +0200

    simplify the client side code even further

--- StarcounterNext/MyPartialToStandaloneHtmlProvider.cs
+++ StarcounterNext/MyPartialToStandaloneHtmlProvider.cs
@@ -1,13 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace HelloWorld {
+﻿namespace HelloWorld {
     class MyPartialToStandaloneHtmlProvider : PartialToStandaloneHtmlProvider {
         public MyPartialToStandaloneHtmlProvider() : base(@"<!DOCTYPE html>
-
 <html>
 <head>
     <meta charset=""utf-8"">
@@ -15,20 +8,23 @@ namespace HelloWorld {
 
     <script src=""/sys/webcomponentsjs/webcomponents.min.js""></script>
     <link rel=""import"" href=""/sys/polymer/polymer.html"">
+    
     <link rel=""import"" href=""/sys/puppet-client/puppet-client.html"">
-
-    <!-- Import Starcounter specific components -->
-    <link rel=""import"" href=""/sys/starcounter-include/starcounter-include.html"">
-    <link rel=""import"" href=""/sys/starcounter-debug-aid/src/starcounter-debug-aid.html"">
+    <link rel=""import"" href=""/sys/imported-template/imported-template.html"">
+    
+    <link rel=""import"" href=""/sys/bootstrap.html"">
     <style>
-    body {{
-      padding: 20px;
-      font-size: 14px;
-    }}
-  </style>
+        body {{
+            margin: 20px;
+        }}
+    </style>
+
+    <link rel=""import"" href=""/sys/starcounter-debug-aid/src/starcounter-debug-aid.html"">
 </head>
 <body>
-    <template is=""dom-bind"" id=""puppet-root""><template is=""imported-template"" content$=""{{{{model.Html}}}}"" model=""{{{{model}}}}""></template></template>
+    <template is=""dom-bind"" id=""puppet-root"">
+        <template is=""imported-template"" content$=""{{{{model.Html}}}}"" model=""{{{{model}}}}""></template>
+    </template>
     <puppet-client ref=""puppet-root""></puppet-client>
     <starcounter-debug-aid></starcounter-debug-aid>
 </body>
