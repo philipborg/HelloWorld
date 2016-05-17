bbe31340e134022092ecbf7fa213f2db823f0782
warpech <warpech@gmail.com>
2016-05-17 19:00:35 +0200

    change FullName to be a computed property

--- PersonJson.json.cs
+++ PersonJson.json.cs
@@ -2,6 +2,8 @@ using Starcounter;
 
 namespace HelloWorld {
     partial class PersonJson : Json {
+        public string _FullName => FirstName + " " + LastName;
+
         void Handle(Input.Save action) {
             Transaction.Commit();
         }
