6cc6e040ce45f1f0a983a352bfa80ce2d50d13c3
Marcin Warpechowski <warpech@gmail.com>
2016-09-29 12:01:54 +0200

    step 8: add mapper app for Images in expenses

---
+++ src/HelloWorldMapper/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Starcounter;
+
+namespace HelloWorldMapper
+{
+    class Program
+    {
+        static void Main()
+        {
+            UriMapping.OntologyMap<HelloWorld.Expense>("/HelloWorld/partial/expense/{?}");
+
+            UriMapping.OntologyMap<HelloWorld.Expense>("/images/concept/{?}");
+        }
+    }
+}
