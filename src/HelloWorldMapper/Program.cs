56406a25dbd5ef82d849478f1991005608c4d869
Marcin Warpechowski <warpech@gmail.com>
2016-09-29 12:01:54 +0200

    step 8: add mapper app for Images in expenses
    In this step, we are using Images app to provide illustrations for each of
    our expenses.
    
    This requires to write a mapper app that maps HelloWorld app to a prefab
    Images app.
    
    It is important to start the apps in the particular order:
    
    1. HelloWorld (current repo)
    2. Images (https://github.com/StarcounterPrefabs/Images)
    3. HelloWorldMapper (current repo)

---
+++ src/HelloWorldMapper/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Starcounter;
+using Starcounter.Internal;
+
+namespace HelloWorldMapper
+{
+    class Program
+    {
+        static void Main()
+        {
+            UriMapping.OntologyMap<HelloWorld.Expense>("/HelloWorld/partial/expense/{?}");
+
+            StarcounterEnvironment.RunWithinApplication("Images", () => {
+                Handle.GET("/images/partials/concept-expense/{?}", (string objectId) => {
+                    return Self.GET("/images/partials/somethings-edit/" + objectId);
+                });
+
+                UriMapping.OntologyMap<HelloWorld.Expense>("/images/partials/concept-expense/{?}");
+            });
+
+
+        }
+    }
+}
