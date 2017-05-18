f4431d1c5308cd72b6ee2a7bf4def4ffa1bdb468
Erlend <elandroe@icloud.com>
2017-05-18 08:44:06 +0200

    move new expense button to personJson for layout

--- src/HelloWorld/ControlsJson.json.cs
+++ src/HelloWorld/ControlsJson.json.cs
@@ -14,19 +14,9 @@ namespace HelloWorld
             Transaction.Rollback();
         }
 
-        void Handle(Input.NewExpenseTrigger action)
-        {
-            new Expense()
-            {
-                Spender = (this.Parent as PersonJson).Data as Spender,
-                Amount = 1
-            };
-        }
-
         void Handle(Input.DeleteAllTrigger action)
         {
-            Db.SlowSQL("DELETE FROM Expense WHERE Spender = ?", this.Parent.Data);
-            (this.Parent as PersonJson).Expenses.Clear();
+            (this.Parent as PersonJson).DeleteAllExpenses();
         }
     }
 }
